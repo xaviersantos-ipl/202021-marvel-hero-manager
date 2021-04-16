c90c9bbce4e8ff0d20e2b718bd5697483f2ba107
Ricardo Gomes <ricardo@bytencode.com>
2021-04-16 18:49:57 +0100

    secrets input removed

--- src/backend/marvel.js
+++ src/backend/marvel.js
@@ -3,8 +3,8 @@ const { createHash } = require('crypto');
 
 //###############
 //CHANGE ME
-const MARVEL_PUBLIC_KEY = process.env.MARVEL_PUBLIC_KEY || 'CHANGEME';
-const MARVEL_PRIVATE_KEY = process.env.MARVEL_PRIVATE_KEY || 'CHANGEME';
+const MARVEL_PUBLIC_KEY = process.env.MARVEL_PUBLIC_KEY;
+const MARVEL_PRIVATE_KEY = process.env.MARVEL_PRIVATE_KEY;
 //###############
 
 
